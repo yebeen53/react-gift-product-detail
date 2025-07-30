06e522ec23cf80d585f91a6925e985998b225868
Yebeen <yebeenlee0503@naver.com>
2025-07-30 14:44:59 +0900

    refactor:상품 상세 정보 key와 alt 수정

--- src/components/productdetail/ProductDetailInfo.tsx
+++ src/components/productdetail/ProductDetailInfo.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { useProductDetail } from '@/hooks/useProduct';
+import theme from '@/data/theme';
 
 interface Props {
   productId: string;
@@ -9,11 +10,11 @@ const ProductDetailInfoInner = ({ productId }: Props) => {
   const { data: detail } = useProductDetail(productId);
 
   return (
-    <section style={{ margin: '1rem' }}>
+    <section style={{ margin: theme.spacing.spacing4 }}>
       <h2>상품설명</h2>
       <div dangerouslySetInnerHTML={{ __html: detail.description }} />
       {detail.images?.map((img: string, idx: number) => (
-        <img key={idx} src={img} alt={`detail-${idx}`} />
+        <img key={img} src={img} alt={`상품 상세 이미지${idx}`} />
       ))}
     </section>
   );
